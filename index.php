a316fddd281bbd4125887fa49cb77a930cfee356
FahriArsiansyah <fahriarsiansyah@gmail.com>
2024-05-19 12:48:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,142 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>apotek nyagak</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        .navbar {
+            position: fixed;
+            background-color: #080808;
+            padding: 10px 20px;
+        }
+
+        .halaman{
+            height: 100vh;
+        }
+    </style>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<section class="halaman">
+
+</section>
+
+
+<section class="table">
+<!-- Table -->
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Dosis</th>
+      <th scope="col">Bentuk_sediaan</th>
+      <th scope="col">Produsen</th>
+      <th scope="col">Tanggal_kadaluwarsa</th>
+      <th scope="col">Harga</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Sanmol Paracetamol</td>
+      <td>500mg</td>
+      <td>Tablet</td>
+      <td>PT Kimia Farma</td>
+      <td>31-12-2025</td>
+      <td>Rp.2.500</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Amoxicillin</td>
+      <td>250mg</td>
+      <td>kapsul</td>
+      <td>PT Sanbe Farma</td>
+      <td>12-12-2024</td>
+      <td>Rp.40.500</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Ibuprofen</td>
+      <td>200mg</td>
+      <td>Tablet</td>
+      <td>PT Kalbe Farma</td>
+      <td>01-01-2025</td>
+      <td>Rp.2.600</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Panadol</td>
+      <td>500mg</td>
+      <td>Kaplet</td>
+      <td>PT Sterling Product Indonesia</td>
+      <td>25-05-2025</td>
+      <td>Rp.14.000</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Bodrex</td>
+      <td>500mg</td>
+      <td>Tablet</td>
+      <td>Tempo Scan Group</td>
+      <td>25-07-2025</td>
+      <td>Rp.11.000</td>
+    </tr>
+  </tbody>
+</table>
+</section>
+</body>
+
+
+
+
+
+
+
+ <!-- javascript -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
